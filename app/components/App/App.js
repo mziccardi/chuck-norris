6fd19cdfcdddee42b3fff398d304fc6b3213b701
Mike Ziccardi <mikeziccardi@gmail.com>
2017-01-24 11:15:50 -0700

    adds a single joke and displays it to main page

--- app/components/App/App.js
+++ app/components/App/App.js
@@ -1,7 +1,7 @@
 import React, { Component } from 'react';
 
 import Button from '../Button/Button'
-import Header form '../Header/Header'
+import Header from '../Header/Header'
 
 export default class Application extends Component {
   constructor() {
@@ -12,10 +12,26 @@ export default class Application extends Component {
     };
   }
 
+  componentDidMount(){
+    const jokeURL = 'http://api.icndb.com/jokes/random'
+    fetch(jokeURL).then((response)=>{
+      return response.json()
+    }).then((data)=>{
+      let randomJoke = data.value.joke
+      
+      this.setState({randomJoke:data.value.joke})
+    })
+  }
+
+
   render(){
     return(
       <div>
         <Header />
+        <div className = 'randomjoke'>
+          {this.state.randomJoke}
+        </div>
       </div>
     )
   }
+}
--- app/index.js
+++ app/index.js
@@ -2,16 +2,10 @@ import React from 'react'
 import { render } from 'react-dom'
 
 // import { Router, Route, IndexRoute, browserHistory} from 'react-router'
-
+import App from '../app/components/App/App'
 import Header from '../app/components/Header/Header'
 import './styles.scss'
 
-const App = () => {
-  return (
-    <div>
-      <Header />
-    </div>
-  );
-}
+
 
 render(<App />, document.querySelector('.application'));
