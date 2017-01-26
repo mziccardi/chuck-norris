fa5e5bbe055182a1365fcf2602e52be475f07613
Mike Ziccardi <mikeziccardi@gmail.com>
2017-01-26 14:57:48 -0700

    starts testing

--- app/components/Header/header.spec.js
+++ app/components/Header/header.spec.js
@@ -1,20 +1,22 @@
 import React from 'react';
 import { mount, shallow } from 'enzyme';
-import {expect} from 'chai';
+import {expect, assert } from 'chai';
 import sinon from 'sinon'
 
 import Header from './Header';
 import Button from '../Button/Button'
 
-describe('<Header/>', function () {
+describe('Header', function () {
   it('should display a single h1 tag', function () {
     const wrapper = shallow(<Header/>);
     expect(wrapper.find('h1')).to.have.length(1);
   });
-  it('should trigger our logStuff function', () => {
-  const logStuff = sinon.spy()
-  const wrapper = mount(<Button handleClick={logStuff} />);
-  wrapper.simulate('click');
-  expect(logStuff.calledOnce).to.equal(true);
-});
+  it('should render as a div',()=>{
+    const wrapper = shallow(<Header />)
+    assert.equal(wrapper.type(),'div')
+  })
+  it('should have a settings button', ()=>{
+    const wrapper = shallow(<Header />)
+    expect(wrapper.find('button')).to.have.length(1)
+  })
 });
